675a10f3ff115de227d5a76ee1d9975d1752e630
Aaron Ortiz <aronortiz90@yahoo.com>
2021-08-11 16:56:05 -0500

    Initial commit

---
+++ Hola_Mundo.py
@@ -0,0 +1,4 @@
+#Para poder imprimir algo en la consola usamos la expresion print(aqui colocamos entre comillas lo que queremos decir)
+#El operador"\n" es para dar un salto de linea
+print("Hola Mundo\n mi nombre es Aaron Ortiz")
+print("Mi nombres es Aaron")
