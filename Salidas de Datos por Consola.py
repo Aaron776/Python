feca3848c15467ea91a82057d000c9fd651dc996
Aaron Ortiz <aronortiz90@yahoo.com>
2021-08-12 13:05:54 -0500

    Actualizar

---
+++ Salidas de Datos por Consola.py
@@ -0,0 +1,9 @@
+nombre="Aaron"
+edad=24
+#Una forma de imprimir los datos en consola diferente al print es con la opcion format()
+print("Hola {} tienes {} años".format(nombre,edad))
+
+#Otra forma es anteponiendo la letra f antes de las comillas
+print(f"Hola {nombre} tienes {edad} años")
+
+
---
+++ Variables.py
@@ -0,0 +1,42 @@
+#En python tenemos la ventaja de no indicarle que tipo de dato es solo  basta con definir la variable
+
+#Numeros Enteros
+numero=1
+print(numero)
+
+#Numeros Decimales con float o double
+numero2=10.8
+print(numero2)
+
+#Cadenas de texto con comillas doble y comillas simples
+letra="a"
+print(letra)
+
+palabra="Estoy 'estudiando'"
+print(palabra)
+
+#Valores Booleanos (True o False)
+palabra2=True
+palabra3=False
+print(palabra2)
+
+#Operaciones Matematicas
+num1=4
+num2=9
+suma=num1+num2*10
+
+
+#Para poder concatenar una variable o algo en vez de poner (+) ponemos la (,)
+print("La suma es:",suma)
+
+#Tipado Dinamico es cuando a una misma variable se le puede asignar una valor
+# de diferente tipo en diferente momento del programa
+
+valor=12
+print("El valor actual de la variable es:",valor)
+
+valor="Aaron"
+print("El valor actual de la variable es:",valor)
+
+
+
