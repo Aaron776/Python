4c68b2633fd3b216caa3d634cdfe1148b3a17dd7
Aaron776 <aronortiz90@yahoo.com>
2021-12-18 16:57:24 -0500

    Actualizar

--- PokeApi.py
+++ PokeApi.py
@@ -1,12 +1,12 @@
 import requests
 def peticionesHttp():
-    url=("https://pokeapi.co/api/v2/pokemon-form/")
-    r=requests.get(url)
+    url = ("https://pokeapi.co/api/v2/pokemon?limit=100&offset=1")
+    r = requests.get(url)
     print(r)     #Con este print lo que hago es imprimir el resultado de mi peticioon Http si es exitosa o no y esto me devuelve un response
     if r.status_code == 200:
-        data=r.json()
+        data = r.json()
         print(data)     #Con este print estoy imprimiendo todo lo que me llega de mi servicio como formato json que en python es un diccionario
         for item in data["results"]:   #En este for simplemento esto recorriendo el atributo "results" que contiene un arreglo de objetos y estoy imprimiendo los valores del atributo name de ese esos objetos
-            print(item["name"])
+            print(item['name'])
 
 peticionesHttp()
