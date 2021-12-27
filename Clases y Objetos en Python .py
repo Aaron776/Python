d380510be1786cb97687cc560c0440b4ae5168ea
Aaron776 <aronortiz90@yahoo.com>
2021-12-26 19:55:23 -0500

    Actualizar

--- Clases y Objetos en Python .py
+++ Clases y Objetos en Python .py
@@ -5,15 +5,26 @@
 #inicializar esa variables o propiedades
 
 class Estudiante:
-    def __init__(self,ci,nombre,apellido,celular):
+    #Atributos de la Clase
+    nombre=""
+    apellido=""
+    ci=""
+
+    def __init__(self,ci,nombre,apellido,celular): #aqui creamos un constructor para inicializar la clase solo si queremos
         self.ci = ci
         self.nombre = nombre
         self.apellido = apellido
         self.celular = celular
 
+    #Metodos o Funciones de la Clase
+    def saludar(self): # cuando creamos una funcion en una clase debemos poner la palabra self en los parentesis para especificar que vamos a trabajr con los atributos de esa clase
+        print("Hola Mundo")
+        print("Hola como estas:", self.nombre)
+
 #Para crear un objeto de esa clase creada lo hacemos de la siguiente manera: ponemos el nombre de nuestro objeto y le igualamos a la clase que creamos
 estudiante1=Estudiante(1725159683,"Aaron","Ortiz","0984588833")
 print(estudiante1.ci)
+estudiante1.saludar()
 
 #Para poder cambiar un dato de mi objeto creado lo hacemos asi:
 estudiante1.nombre="Patricio"
