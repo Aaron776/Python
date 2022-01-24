d8de63601a5f62fe9b0479d45a3408cd659fc964
Aaron776 <aronortiz90@yahoo.com>
2022-01-24 10:49:46 -0500

    Actualizar

--- Conexion a BD MYSQL.py
+++ Conexion a BD MYSQL.py
@@ -1,11 +1,11 @@
 #Para conectar python a mysql debemos importar la liberia pymsql mediante el comando:  mysql-connector-python
 import pymysql
 try:
-    conexionSQ=pymysql.connect(host='localhost', user='root',password='',db="unibe")
+    conexionSQ=pymysql.connect(host='localhost', user='root',password='',db="sana")
     cursor=conexionSQ.cursor()
     print("Conexion Exitosa")
 
-    # Aqui solamente estoy obteniendo la vertsion de mi MYSQL
+    # Aqui solamente estoy obteniendo la version de mi MYSQL
     cursor = conexionSQ.cursor()
     cursor.execute("SELECT version()")
     fila = cursor.fetchone()
@@ -17,6 +17,15 @@ try:
     registros = cursor.fetchall()  # usamos el metodo fetchall() para obtener todos los rgistros de una tabla y el metodo fecthone() es para obtener un solo registro de la tabla
     for item in registros:  # para imprimir los registro de una tabla usamo un for in
         print(item)
+
+        # Insertar datos o registros a la base de datos
+    cursor = conexionSQ.cursor()
+    id_alumno = int(input("Ingresar id del alumno: "))
+    nombre = input("Ingresar nombre: ")
+    apellido = input("Ingresar apellido: ")
+    cursor.execute("INSERT INTO alumnos (id_alumno,nombre,apellido) VALUES(id_alumno,'nombre','apellido')")
+    conexionSQ.commit()
+    print("Registro de datos exitoso")
 except:
     print("Error en la Conexion")
 
